61b98760995608ccc318b8f6b2335d135cf4065d
Thalisu <thalisondmcc@gmail.com>
2024-11-26 20:37:33 -0300

    refactor: adjust to use PDFDataFormatter class

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import getData from "./scripts/getData.js";
+import PDFDataFormatter from "./scripts/PDFDataFormatter.js";
 import formatData from "./scripts/formatData.js";
 
 const form = document.querySelector(".formContainer");
@@ -7,33 +7,28 @@ form.addEventListener("submit", (e) => handleSubmit(e));
 function handleSubmit(e) {
   e.preventDefault();
 
-  const extrato = document.querySelector("#textArea");
-  const rawData = extrato.value.split(/[\s\n]+/);
-  const data = getData(rawData);
+  const bank = JSON.parse(document.querySelector("#banco").value);
 
-  const banco = document.querySelector("#banco").value;
-  const bankId = document.querySelector("#bankId").value;
+  const rawData = document.querySelector("#textArea")?.value;
+  if (!rawData) return;
+
+  const ano = document.querySelector("#ano").value;
+
+  const formatter = new PDFDataFormatter(rawData, bank.name, ano);
+
+  const data = formatter.get(rawData);
 
   const agencia = document.querySelector("#agencia");
   const conta = document.querySelector("#conta");
   const account = `${agencia.value}${conta.value.replace(/[.,-]/g, "")}`;
-  console.log(account);
 
-  const ano = document.querySelector("#ano").value;
+  const firstDate = `${data[0].date.length === 8 ? "" : ano}${data[0].date}`;
 
-  const firstData = `${ano}${data[0][0].replace("/", "")}`;
-  const lastData = `${ano}${data[data.length - 1][0].replace("/", "")}`;
+  const lastDate = `${data[data.length - 1].date.length === 8 ? "" : ano}
+  ${data[data.length - 1].date}
+  `;
 
   const saldo = document.querySelector("#saldo").value;
 
-  formatData(
-    data,
-    banco,
-    bankId,
-    account,
-    ano,
-    firstData,
-    lastData,
-    saldo
-  );
+  formatData(data, bank.name, bank.id, account, firstDate, lastDate, saldo);
 }
